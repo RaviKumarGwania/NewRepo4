c7694058da02c586e7606b5920003df88f366fc6
RAVI KUMAR <RAVI KUMAR>
2016-09-19 15:53:53 +0530

    Hey Ravi added a new line of code in local repository

--- NewRepo4/Main.java
+++ NewRepo4/Main.java
@@ -4,6 +4,7 @@ class
 	{
 		System.out.println("Hello World!");
 		System.out.println("Welcome to Git World!");
-		System.out.println("Hey i am in Git World.com ")
+		System.out.println("Hey i am in Git World.com ");
+		System.out.println("Hey i am added a new branch in local repository");
 	}
 }
